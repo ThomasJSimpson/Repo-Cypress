530466aa6b58a3aea88aab01303e29abd3649d36
Thomas Berenger <berenger.thomas@gmail.com>
2026-01-07 17:01:12 +0100

    Vérif valeur prix en cours

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,22 +1,13 @@
 import "cypress-real-events";
 
 describe("connexion + recherche + ajout panier + vérif panier", () => {
-  // beforeEach(() => {
-  //   cy.visit(Cypress.env("site_url"));
-  //   cy.get("#username").type(Cypress.env("db_user"));
-  //   cy.get("#password").type(Cypress.env("db_password"), { log: false });
-  //   cy.get("#rememberme").click();
-  //   cy.get(".woocommerce-form-login__submit").click();
-  // });
-  it("se connecte", () => {
-    // Rejoinds le site
+  it("fait tout d'un coup (pour le moment...)", () => {
+    // ÉTAPE 1 : CONNEXION
+    cy.log("ÉTAPE 1 : Connexion");
     cy.visit(Cypress.env("site_url"));
     cy.get(".page-title-text").contains("Boutique");
-    // Rejoinds page de connexion
     cy.get(".menu-item-2772").click();
     cy.get(".page-title-text").contains("Mon compte");
-
-    // Entre les crédentials et se co
     cy.get("#username").type(Cypress.env("db_user"));
     cy.get("#password").type(Cypress.env("db_password"), { log: false });
     cy.get("#rememberme").click();
@@ -24,19 +15,31 @@ describe("connexion + recherche + ajout panier + vérif panier", () => {
     cy.get(".woocommerce-MyAccount-content p")
       .first()
       .should("contain", `Bonjour ${Cypress.env("db_user")}`);
-    // Recherche l'article et clique dessus vers page produit
+    // ÉTAPE 2 : RECHERCHE
+    cy.log("ÉTAPE 2 : Recherche produit");
     cy.get(".search-overlay-trigger").click();
     cy.get(".apsw-search-input").type(Cypress.env("product"));
     cy.get("ul.apsw_data_container li").first().find(".apsw-name").should("contain", Cypress.env("product"));
     cy.get("ul.apsw_data_container li").first().click();
-    //Vérifie bonne fiche produit
     cy.get(".product_title").first().should("contain", Cypress.env("product"));
-    // Ajoute le prod au panier
+    // ÉTAPE 3 : PANIER
+    cy.log("ÉTAPE 3 : Ajout panier");
     cy.get(".single_add_to_cart_button").click();
-    // Va voir le récap de la commande
+    // Va voir le récap de la commande dans le panier
     cy.get(".wc-forward").click();
-    // Se déco via le menu déroulant "hover"
-    cy.get(".header-icon").find(".icofont-user-alt-4").realHover();
-    cy.get(".header-icon").find(".woocommerce-MyAccount-navigation-link--customer-logout").click();
+    cy.wait(2500);
+    cy.get(".wp-block-woocommerce-cart-line-items-block")
+      .find("tbody")
+      .children()
+      .then((enfants) => {
+        // npombre d'article différents
+        const nombreDenfants = enfants.length;
+        cy.log("Le nombre d'enfants est : " + nombreDenfants);
+      });
+
+    // // ÉTAPE 4 : DÉCONNEXION
+    // cy.log("ÉTAPE 4 : Déconnexion");
+    // cy.get(".header-icon").find(".icofont-user-alt-4").realHover();
+    // cy.get(".header-icon").find(".woocommerce-MyAccount-navigation-link--customer-logout").click();
   });
 });
---
+++ functions.js
@@ -0,0 +1,14 @@
+function multiplication(a, b) {
+  let resultat = a * b;
+  return resultat;
+}
+
+const addition = (a, b) => {
+  let resultat = a + b;
+  return resultat;
+};
+
+const monResultatMultiplication = multiplication(2, 5);
+const monResultatSomme = somme(2, 5);
+console.log(monResultatMultiplication);
+console.log(monResultatSomme);
